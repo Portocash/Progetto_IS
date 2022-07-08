b17e5ab962b831b433c742c0e9c390018d9b852c
Carlo Portosalvo <carloportosalvo@localhost>
2022-07-08 23:05:32 +0200

    Primo commit
    Signed-off-by: Carlo Portosalvo <carloportosalvo@localhost>

---
+++ ElaboratoLCM/src/main/principale.java
@@ -0,0 +1,13 @@
+package main;
+
+public class principale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		int a = 5;
+		System.out.println(a);
+
+	}
+
+}
