f21cece70d355bdc0e71047f336f0a67967ee6ae
Carlo Portosalvo <carloportosalvo@localhost>
2022-07-08 23:54:27 +0200

    Aggiunto un commento e un sout

--- ElaboratoLCM/src/main/principale.java
+++ ElaboratoLCM/src/main/principale.java
@@ -3,10 +3,11 @@ package main;
 public class principale {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		int a = 5;
 		System.out.println(a);
+		//new branch locale ahahaha
+		System.out.println("Figata");
 
 	}
 
