4b5d29534a39def7802308393f41658c6e821ea0
Carlo Portosalvo <carloportosalvo@localhost>
2022-07-11 16:31:14 +0200

    Definizione DBManager

--- ElaboratoLCM/src/database/DBManager.java
+++ ElaboratoLCM/src/database/DBManager.java
@@ -1,5 +1,33 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class DBManager {
+	
+	private static Connection conn = null;
+	
+	private DBManager() {}
+	
+	public static Connection getConnection() throws SQLException {
+			
+			if(conn == null || conn.isClosed()) {
+				conn = DriverManager.getConnection("jdbc:mariadb://192.168.195.139:3306/GestioneIstitutoScolastico",
+				        "carlo", "carlo");
+			}
+			
+			return conn;
+		
+	}
+	
+	
+	public static void closeConnection() throws SQLException {
+		
+			if(conn != null) {
+				conn.close();
+			}
+	}
 
 }
