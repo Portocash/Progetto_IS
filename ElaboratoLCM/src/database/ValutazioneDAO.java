7c836b13736f930903ae8bff82a84e82974012ed
marty <marty@DESKTOP-0J3L9IM.station>
2022-07-13 12:19:26 +0200

    valutazioe

--- ElaboratoLCM/src/database/ValutazioneDAO.java
+++ ElaboratoLCM/src/database/ValutazioneDAO.java
@@ -2,4 +2,149 @@ package database;
 
 public class ValutazioneDAO {
 
+	
+	package database;
+
+	import java.sql.Connection;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+
+import entity.EntityAlunno;
+import entity.EntityValutazione;
+	import exception.DAOException;
+	import exception.DBConnectionException;
+
+	public class ValutazioneDAO {
+
+		public static void createValutazione(EntityValutazione eV) throws DAOException, DBConnectionException {
+			
+			try {
+				
+				Connection conn = DBManager.getConnection();
+
+				String query = "INSERT INTO VALUTAZIONI VALUES (?,?,?,?,?,?);";
+
+				try {
+					PreparedStatement stmt = conn.prepareStatement(query);
+					
+					stmt.setInt(1,eV.getId());
+					stmt.setDate(2, eV.getData());
+					stmt.setString(3, eV.getMateria());
+					stmt.setInt(4, eV.getAlunno().getMatricola());
+					stmt.setInt(5, eV.getIdRegistroElettronico());
+					stmt.setInt(6,eV.getVoto());
+
+					stmt.executeUpdate();
+
+				}catch(SQLException e) {
+					throw new DAOException("Errore scrittura biglietto");
+				} finally {
+					DBManager.closeConnection();
+				}
+
+			}catch(SQLException e) {
+				throw new DBConnectionException("Errore connessione database");
+			}
+
+		}
+
+		public static EntityValutazione readValutazione(int id_voto, int matricola_alunno) throws DAOException, DBConnectionException {
+
+			EntityValutazione eV = null;
+			EntityAlunno ea = new EntityAlunno("","", null, "", "", "", "", "", "", matricola_alunno);
+
+
+			try {
+
+				Connection conn = DBManager.getConnection();
+
+				try {
+					String query = "SELECT * FROM VALUTAZIONE WHERE ID_VOTO = ? ;";
+
+
+					PreparedStatement stmt = conn.prepareStatement(query);
+					
+					
+					stmt.setInt(1,id_voto);
+
+					ResultSet result = stmt.executeQuery();
+
+					if(result.next()) {
+						eV = new EntityValutazione(result.getInt(1),result.getDate(2),result.getString(3),result.getInt(5), result.getInt(6),ea);	
+					}
+
+				}catch(SQLException e) {
+					throw new DAOException("Errore lettura valutazione");
+				}finally {
+					DBManager.closeConnection();
+				}
+				
+			}catch(SQLException e) {
+				throw new DBConnectionException("Errore di connessione DB");
+			}
+
+			return eV;
+		}
+
+		public static void updateValutazione(EntityValutazione eV) throws DAOException, DBConnectionException {
+			
+
+			try {
+
+				Connection conn = DBManager.getConnection();
+
+				try {
+					String query = "UPDATE GENITORI SET DATA=?, MATERIA=?, MATRICOLAALUNNO=?, ID_REGISTROELETTRONICO=?, VOTO=? WHERE ID_VOTO=? ;";
+
+
+					PreparedStatement stmt = conn.prepareStatement(query);
+					
+					stmt.setDate(1, eV.getData());
+					stmt.setString(2, eV.getMateria());
+					stmt.setInt(3, eV.getAlunno().getMatricolaAlunno());
+					stmt.setInt(4, eV.getIdRegistroElettronico());
+					stmt.setInt(5,eV.getVoto());
+					stmt.setInt(6,eV.getId());
+
+					stmt.executeUpdate();
+
+				}catch(SQLException e) {
+					throw new DAOException("Errore update valutazione");
+				}finally {
+					DBManager.closeConnection();
+				}
+				
+			}catch(SQLException e) {
+				throw new DBConnectionException("Errore di connessione DB");
+			}
+
+		}
+
+
+		public static void deleteGenitore(int id_voto) throws DAOException, DBConnectionException {
+			
+			
+			try {
+
+				Connection conn = DBManager.getConnection();
+
+				String query = "DELETE FROM VALUTAZIONI WHERE ID_VOTO = ?; ";
+
+				try {
+					PreparedStatement stmt = conn.prepareStatement(query);
+					stmt.setInt(1,id_voto); 
+					stmt.executeUpdate();
+
+				}catch(SQLException e) {
+					throw new DAOException("Errore cancellazione valutazione");
+				} finally {
+					DBManager.closeConnection();
+				}
+
+			}catch(SQLException e) {
+				throw new DBConnectionException("Errore di connessione DB");
+			}
+		}
+	}
 }
--- ElaboratoLCM/src/entity/EntityValutazione.java
+++ ElaboratoLCM/src/entity/EntityValutazione.java
@@ -1,5 +1,67 @@
 package entity;
 
-public class EntityValutazione {
+import java.util.Date;
+import database.ValutazioneDAO;
+import exception.DBConnectionException;
 
-}
+
+	public class EntityValutazione{
+		private int id_voto;
+		private Date data;
+		private String materia;
+		private int id_RegistroElettronico;
+		private int voto;	
+		private EntityAlunno studente;
+		
+
+		public EntityValutazione(int id, Date data, String materia, int idRegistroElettronico, int voto, EntityAlunno ea) {
+			super();
+			this.id_voto= id;
+			this.data= data;
+			this.materia=materia;
+			this.id_RegistroElettronico=idRegistroElettronico;
+			this.voto=voto;
+			this.studente=ea;
+		}
+		
+		public EntityAlunno getAlunno() {
+			
+			return studente;
+		}
+		public void associaStudente(EntityAlunno ea){ this.studente=ea;}
+		public void rimuoviStudente() { this.studente=null; }
+		
+		public int getId() {
+			return id_voto;
+		}
+		public void setId(int id){
+			this.id_voto=id;
+		}
+		public date getData() {
+			return data;
+		}
+		public void setData(date data){
+			this.data=data;
+		}
+		public String getMateria() {
+			return materia;
+		}
+		public void setMateria(String materia){
+			this.materia=materia
+		}
+	
+		public int getIdRegistroElettronico() {
+			return id_RegistroElettronico;
+		}
+		public void setIdRegistroElettronico(int idRegistro){
+			this.id_RegistroElettronico=idRegistro;
+		}
+		public int getVoto() {
+			return voto;
+		}
+		public void setVoto(int voto){
+			this.voto=voto;
+		}
+	}
+
+	
